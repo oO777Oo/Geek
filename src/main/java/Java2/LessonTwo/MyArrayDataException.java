c04d8fe26d6bfb1390baa70c47eb87bd45834628
Vladislav <vladislav.popovici96@gmail.com>
2021-02-23 17:12:13 +0100

    homework

---
+++ src/main/java/Java2/LessonTwo/MyArrayDataException.java
@@ -0,0 +1,12 @@
+package Java2.LessonTwo;
+
+public class MyArrayDataException extends Exception {
+
+    public MyArrayDataException (String message) {
+        super(message);
+    }
+
+    public String getMessage(int x, int y) {
+        return "Convert data exception with coordinates (" + x + "," + y +") ";
+    }
+}
---
+++ src/main/java/Java2/LessonTwo/MyArraySizeException.java
@@ -0,0 +1,11 @@
+package Java2.LessonTwo;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+    public String getMessage() {
+        return "My array size exception";
+    }
+}

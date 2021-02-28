eb7229935bac9c49abb501e8bde5d2398c751b05
Vladislav <vladislav.popovici96@gmail.com>
2021-02-28 21:52:22 +0100

    change 2 methods into labda functions

--- src/main/java/Java2/LessonFour/Main.java
+++ src/main/java/Java2/LessonFour/Main.java
@@ -23,19 +23,16 @@ public class Main {
     }
 
     public static String reverse(String s) {
-        Reverse reverse1 = new Reverse() {
-            @Override
-            public String reverse(String s) {
-                char[] str = s.toCharArray();
-                int start = 0;
-                while (start < str.length / 2) {
-                    char current = str[start];
-                    str[start] = str[str.length - 1 - start];
-                    str[str.length - 1 - start] = current;
-                    start++;
-                }
-                return new String(str);
+        Reverse reverse1 = s1 -> {
+            char[] str = s1.toCharArray();
+            int start = 0;
+            while (start < str.length / 2) {
+                char current = str[start];
+                str[start] = str[str.length - 1 - start];
+                str[str.length - 1 - start] = current;
+                start++;
             }
+            return new String(str);
         };
         return reverse1.reverse(s);
     }
@@ -60,17 +57,14 @@ public class Main {
     }
 
     public static List<String> search(List<String> list) {
-        Conditional conditional = new Conditional() {
-            @Override
-            public List<String> condition(List<String> s) {
-                List<String> ans = new ArrayList<>();
-                for (String str : s) {
-                    if (str.length() == 3 && str.charAt(0) == 97) {
-                        ans.add(str);
-                    }
+        Conditional conditional = s -> {
+            List<String> ans = new ArrayList<>();
+            for (String str : s) {
+                if (str.length() == 3 && str.charAt(0) == 97) {
+                    ans.add(str);
                 }
-                return ans;
             }
+            return ans;
         };
         return conditional.condition(list);
     }
